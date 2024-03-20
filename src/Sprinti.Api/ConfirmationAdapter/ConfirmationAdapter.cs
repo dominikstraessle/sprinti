39ed28fac2cc1085188f34fe34109b6eecd9ea0d
Leo Hrvat <hrvat.leo@gmail.com>
2024-03-20 16:04:17 +0100

    Impl Methods for StartAsync and Confirmation

---
+++ src/Sprinti.Api/ConfirmationAdapter/ConfirmationAdapter.cs
@@ -0,0 +1,82 @@
+﻿using Sprinti.Domain;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+using System.Text;
+
+namespace Sprinti.Api.Button
+{
+    public class ConfirmationAdapter : IConfirmationAdapter
+    {
+        private static readonly HttpClient _client = new HttpClient();
+        public ILogger<ConfirmationAdapter>? _logger;
+
+        public void Confirmation(CubeConfig cubeConfig)
+        {
+            var config = cubeConfig;
+            var configDictionary = new SortedDictionary<int, Color>();
+            foreach (var entry in config.Config)
+            {
+                configDictionary.Add(entry.Key, entry.Value);
+            }
+            var ConfigModel = new CubeConfig
+            {
+                Time = DateTime.Now,
+                Config = configDictionary
+            };
+
+            string jsonString = JsonConvert.SerializeObject(ConfigModel);
+            PostConfig(jsonString);
+
+        }
+
+        private async void PostConfig(string jsonString)
+        {
+            var Url = new Uri(@"URL/cubes/teamxx/config");
+            using var httpClient = new HttpClient();
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            try
+            {
+                var response = await httpClient.PostAsync(Url, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Successfully posted the config.");
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    _logger.LogInformation(responseBody);
+                }
+                else
+                {
+                    _logger.LogError($"Failed to post the config. Status code: {response.StatusCode}");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                _logger.LogError($"{e.Message}");
+            }
+
+        }
+
+        public async Task<string> StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var uri = new Uri(@"http://52.58.217.104:5000/cubes");
+                HttpResponseMessage response = await _client.GetAsync(uri, cancellationToken);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine("Request was canceled.", e.Message);
+                return "Request was canceled.";
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Message :{0} ", ex.Message);
+                return $"Error: {ex.Message}";
+            }
+        }
+
+    }
+}
