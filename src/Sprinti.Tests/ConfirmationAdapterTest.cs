2279f65a15157f9f929dcd8c4328b6d81e1bf99b
Leo Hrvat <hrvat.leo@gmail.com>
2024-03-20 16:04:42 +0100

    Removed old Folder and added Tests

---
+++ src/Sprinti.Tests/ConfirmationAdapterTest.cs
@@ -0,0 +1,26 @@
+﻿using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using Sprinti.Api.Button;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprinti.Tests
+{
+    public class ConfirmationAdapterTest
+    {
+        [Fact]
+        public async Task StartAsync_ReturnsExpectedResponseBody()
+        {
+            var ConfirmationAdapter = new ConfirmationAdapter();
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            string responseBody = await ConfirmationAdapter.StartAsync(cancellationTokenSource.Token);
+
+            Assert.NotNull(responseBody);
+            Console.WriteLine($"Response Body: {responseBody}");
+        }
+    }
+}
