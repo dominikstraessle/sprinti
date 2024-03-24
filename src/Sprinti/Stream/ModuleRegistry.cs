9f9a4858aa6808f8c4eeb4cdaa8adbd25c89cccd
Dominik Strässle <dominik@straessle.me>
2024-03-24 18:52:08 +0100

    Add VideoCapture service for video streams
    Refactored Stream module to register VideoCapture as a transient service, enabling the VideoStream to utilize it for frame captures. This increases modularity, as the VideoStream class no longer manages video capture initialization on its own.

--- src/Sprinti/Stream/ModuleRegistry.cs
+++ src/Sprinti/Stream/ModuleRegistry.cs
@@ -1,9 +1,18 @@
+using Microsoft.Extensions.Options;
+using OpenCvSharp;
+
 namespace Sprinti.Api.Stream;
 
 public static class ModuleRegistry
 {
     public static void AddStreamModule(this IServiceCollection services)
     {
+        services.AddTransient<VideoCapture>(provider =>
+        {
+            var options = provider.GetRequiredService<IOptions<StreamOptions>>();
+            var capture = new VideoCapture(options.Value.RtspSource);
+            return capture;
+        });
         services.AddHostedService<VideoStream>();
     }
 }
--- src/Sprinti/Stream/VideoStream.cs
+++ src/Sprinti/Stream/VideoStream.cs
@@ -3,40 +3,42 @@ using OpenCvSharp;
 
 namespace Sprinti.Api.Stream;
 
-public class VideoStream(ILogger<VideoStream> logger, IOptions<StreamOptions> options) : BackgroundService
+public class VideoStream(VideoCapture capture, ILogger<VideoStream> logger, IOptions<StreamOptions> options)
+    : BackgroundService
 {
     private int _imageIndex;
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation("Started Reader");
-        return Task.Run(async () =>
+        return Task.Run(async () => { await CaptureFrames(stoppingToken); }, stoppingToken);
+    }
+
+    private async Task CaptureFrames(CancellationToken stoppingToken)
+    {
+        var currentWorkingDir = Directory.GetCurrentDirectory();
+        var imageDirectory = Path.Combine(currentWorkingDir, options.Value.SaveImagePathFromProjectRoot);
+        Directory.CreateDirectory(imageDirectory);
+        logger.LogInformation("Created new image directory: {path}", imageDirectory);
+
+        using var image = new Mat();
+
+        // When the movie playback reaches end, Mat.data becomes NULL.
+        while (!stoppingToken.IsCancellationRequested)
         {
-            var currentWorkingDir = Directory.GetCurrentDirectory();
-            var imageDirectory = Path.Combine(currentWorkingDir, options.Value.SaveImagePathFromProjectRoot);
-            Directory.CreateDirectory(imageDirectory);
-            logger.LogInformation("Created new image directory: {path}", imageDirectory);
-
-            var capture = new VideoCapture(options.Value.RtspSource);
-            using var image = new Mat();
-
-            // When the movie playback reaches end, Mat.data becomes NULL.
-            while (!stoppingToken.IsCancellationRequested)
+            capture.Read(image);
+            if (_imageIndex % options.Value.CaptureIntervalInSeconds == 0)
             {
-                capture.Read(image);
-                if (_imageIndex % options.Value.CaptureIntervalInSeconds == 0)
-                {
-                    var imageFilePath = Path.Combine(imageDirectory, $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.png");
-                    image.SaveImage(imageFilePath);
-                    logger.LogInformation("Received image: {rows}x{cols}, saved to {path}", image.Rows, image.Cols,
-                        imageFilePath);
-                }
-
-                _imageIndex += 1;
-                _imageIndex %= options.Value.CaptureIntervalInSeconds;
-                await Task.Delay(1000, stoppingToken);
+                var imageFilePath = Path.Combine(imageDirectory, $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.png");
+                image.SaveImage(imageFilePath);
+                logger.LogInformation("Received image: {rows}x{cols}, saved to {path}", image.Rows, image.Cols,
+                    imageFilePath);
             }
-        }, stoppingToken);
+
+            _imageIndex += 1;
+            _imageIndex %= options.Value.CaptureIntervalInSeconds;
+            await Task.Delay(1000, stoppingToken);
+        }
     }
 
     public override async Task StopAsync(CancellationToken cancellationToken)
