b2a77c1f4889f1cd62bc16148ea5606f39f77e41
Dominik Strässle <dominik@straessle.me>
2024-03-28 11:30:44 +0100

    Simplify JsonConverter attribute in Color enum
    The change introduced simplifies the JsonConverter attribute in the Color enum by including the Json.Serialization namespace at the top. This way, we replace the specific attribute call with a shorter version. As a result, the code becomes more readable, while its functionality remains the same.

--- src/Sprinti/Domain/Color.cs
+++ src/Sprinti/Domain/Color.cs
@@ -1,10 +1,11 @@
+using System.Text.Json.Serialization;
 using Sprinti.Confirmation;
 
 namespace Sprinti.Domain;
 
 // The value represents the index of the color-depot in the default/reset state
 // Do not touch unless the colors are stored in another formation
-[System.Text.Json.Serialization.JsonConverter(typeof(JsonConverters.ColorJsonConverter))]
+[JsonConverter(typeof(JsonConverters.ColorJsonConverter))]
 public enum Color
 {
     None = 0,
