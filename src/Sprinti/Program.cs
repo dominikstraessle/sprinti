610e02b3121c90663348bfa944daf22db8c55ade
Dominik Strässle <dominik@straessle.me>
2024-04-11 10:41:01 +0200

    Update Swagger UI API name
    The main action in this commit is an update to the Swagger UI API name from "Your API V1" to "Sprinti". This change makes the API name more specific to the application, enhancing readability and clarity.

--- src/Sprinti/Program.cs
+++ src/Sprinti/Program.cs
@@ -42,7 +42,7 @@ public static class Program
         // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint
         app.UseSwaggerUI(c =>
         {
-            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sprinti");
             c.RoutePrefix = "";
         });
 
