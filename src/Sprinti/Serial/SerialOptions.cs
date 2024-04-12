ec4c4a105dee1bbc68ae5ca3944ac9ba5506bbf4
Dominik Strässle <dominik@straessle.me>
2024-04-12 10:33:10 +0200

    Disable Stream feature and update serial port configuration
    The Stream feature has been disabled via the appsettings.json file. Moreover

--- src/Sprinti/Serial/SerialOptions.cs
+++ src/Sprinti/Serial/SerialOptions.cs
@@ -6,7 +6,7 @@ public class SerialOptions
 {
     public const string Serial = "Serial";
     public bool Enabled { get; set; }
-    public string PortName { get; set; } = "/dev/pts/1";
+    public string PortName { get; set; } = "/dev/pts/0";
     public int BaudRate { get; init; } = 115200;
     public Parity Parity { get; init; } = Parity.None;
     public int DataBits { get; init; } = 8;
