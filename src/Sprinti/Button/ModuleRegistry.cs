afd73df2212f4fcb2749073d5561a19c9e603efb
Dominik Strässle <dominik@straessle.me>
2024-04-14 21:31:33 +0200

    Add DisplayService with new Display module and update button module
    This commit introduces a new DisplayService within a new Display module which includes DisplayOptions and ModuleRegistry. It also updates the Button module in the ModuleRegistry with new GPIO usage details and platform-specific constraints. The DisplayService addition enhances the project with a decoupled and testable display handling structure.

--- src/Sprinti/Button/ModuleRegistry.cs
+++ src/Sprinti/Button/ModuleRegistry.cs
@@ -9,7 +9,10 @@ public static class ModuleRegistry
 {
     public static void AddButtonModule(this IServiceCollection services)
     {
-        // The button must be transient and only used a single time. Otherwise the
+        // pinctrl poll 26
+        // gpioinfo -> use gpio chip 4 (https://github.com/dotnet/iot/issues/2262#issuecomment-1890441048)
+        // use libgpiod v1 (v2 not supported at the moment) (https://github.com/dotnet/iot/issues/2179)
+        // Pi5 Board not supported at the moment
         services.AddScoped<GpioDriver>(provider =>
             new LibGpiodDriver(provider.GetRequiredService<IOptions<ButtonOptions>>().Value.GpioChip));
         services.AddScoped<GpioController>(provider =>
---
+++ src/Sprinti/Display/ModuleRegistry.cs
@@ -0,0 +1,12 @@
+using System.Device.I2c;
+
+namespace Sprinti.Display;
+
+public static class ModuleRegistry
+{
+    public static void AddDisplayModule(this IServiceCollection services)
+    {
+        var blub = I2cDevice.Create(new I2cConnectionSettings(1, 0x7C));
+        services.AddScoped<IDisplayService, DisplayService>();
+    }
+}
