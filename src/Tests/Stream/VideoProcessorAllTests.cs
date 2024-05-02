97070911bcce944cd09f5897c38f10794ea1e5a1
Dominik Strässle <dominik@straessle.me>
2024-05-02 10:47:21 +0200

    Refactor detection.json for improved readability
    The structure of the detection.json file has been reorganized. This reorganization mainly includes improving the arrangement of array elements and moving the "Filename" attributes to the end of each section for consistent structure and enhanced readability.

--- src/Tests/Stream/VideoProcessorAllTests.cs
+++ src/Tests/Stream/VideoProcessorAllTests.cs
@@ -109,7 +109,7 @@ public class VideoProcessorAllTests(
     }
 
     [Theory]
-    [InlineData(0, 20)]
+    [InlineData(0, 0)]
     public async void MovePoints(int x, int y)
     {
         var files = new[]
