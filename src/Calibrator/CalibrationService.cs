abac64741449fb409f40e141043d57c0597581e8
Dominik Strässle <dominik@straessle.me>
2024-05-03 10:51:07 +0200

    Refactor CaptureFrames method and add JSON serialization
    The CaptureFrames method in the Calibration Service has been refactored for better usability. Instead of being asynchronous, it now operates in a synchronous context with the use of Task.Run. Furthermore, new functionality has been included to serialize the captured frames' configurations and save them as a JSON file, enhancing data persistence and retrieval.

--- src/Calibrator/CalibrationService.cs
+++ src/Calibrator/CalibrationService.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -18,10 +19,10 @@ public class CalibrationService(
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation("Started Reader");
-        return CaptureFrames(stoppingToken);
+        return Task.Run(() => CaptureFrames(stoppingToken), stoppingToken);
     }
 
-    private async Task CaptureFrames(CancellationToken stoppingToken)
+    private void CaptureFrames(CancellationToken stoppingToken)
     {
         _stoppingToken = stoppingToken;
         var imageDirectory = Path.Combine(environment.ContentRootPath, options.Value.ImagePathFromContentRoot);
@@ -30,6 +31,7 @@ public class CalibrationService(
 
         using var image = new Mat();
         var frameCount = 0;
+        IList<LookupConfig> configs = [];
 
         while (!stoppingToken.IsCancellationRequested)
         {
@@ -40,11 +42,12 @@ public class CalibrationService(
             }
 
             if (frameCount++ % options.Value.CaptureIntervalInFrames != 0) continue;
-            var imageFilePath = Path.Combine(imageDirectory, $"{DateTime.Now:yyyyMMddHHmmss}.png");
+            var filename = $"{DateTime.Now:yyyyMMddHHmmss}.png";
+            var imageFilePath = Path.Combine(imageDirectory, filename);
             image.SaveImage(imageFilePath);
             logger.LogInformation("Received image: {Rows}x{Cols}, saved to {Path}", image.Rows, image.Cols,
                 imageFilePath);
-            var windowService = new WindowService(imageFilePath, image);
+            var windowService = new WindowService(filename, image);
             var addedConfig = windowService.Calibrate(stoppingToken);
 
             if (addedConfig is null)
@@ -52,10 +55,17 @@ public class CalibrationService(
                 continue;
             }
 
+            configs.Add(addedConfig);
+
             logger.LogInformation("Add config: {Config}", addedConfig);
         }
 
         Cv2.DestroyAllWindows();
+        var updatedDetection = JsonSerializer.Serialize(new DetectionOptions()
+        {
+            LookupConfigs = configs
+        });
+        File.WriteAllText(Path.Combine(imageDirectory, "new.json"), updatedDetection);
     }
 
 
