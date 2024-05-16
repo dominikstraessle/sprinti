a9e40f33d5fca9fd462500eccf6b329e838bac48
Dominik Strässle <dominik@straessle.me>
2024-05-16 10:18:33 +0200

    Added new detection options file and updated stream host
    An additional detection options file has been created and included in the project. This will allow more accurate localization and detection. In addition, the host of the StreamOptions has been modified to 'pren_profile_med'.

--- src/Sprinti/Stream/StreamOptions.cs
+++ src/Sprinti/Stream/StreamOptions.cs
@@ -5,7 +5,7 @@ public class StreamOptions : ISprintiOptions
     public const string Stream = "Stream";
     public string Username { get; set; } = "pren";
     public string Password { get; set; } = "463997";
-    public string Host { get; set; } = "147.88.48.131/axis-media/media.amp?streamprofile=pren_profile_small";
+    public string Host { get; set; } = "147.88.48.131/axis-media/media.amp?streamprofile=pren_profile_med";
     public string RtspSource => $"rtsp://{Username}:{Password}@{Host}";
     public bool Enabled { get; set; } = true;
 }
