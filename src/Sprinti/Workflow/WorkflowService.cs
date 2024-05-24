3ffed5fc2d13109e5eef3d7e06ca4b3989dc5ac6
Dominik Strässle <dominik@straessle.me>
2024-05-24 10:43:12 +0200

    Refactor power calculation in WorkflowService
    The power calculation in WorkflowService has been simplified. Previously, the value was calculated using a division and a power operation; now, the same result is obtained by dividing directly by 3600000, improving readability and performance.

--- src/Sprinti/Workflow/WorkflowService.cs
+++ src/Sprinti/Workflow/WorkflowService.cs
@@ -40,6 +40,6 @@ public class WorkflowService(
         displayService.UpdateProgress(6, $"instructions completed: consumed power {powerInWattHours}");
         var endTask = confirmationService.EndAsync(cancellationToken);
         await Task.WhenAll(startTask, confirmTask, endTask);
-        displayService.UpdateProgress(7, $"{powerInWattHours / 3.6 * Math.Pow(10, 6)}Wh");
+        displayService.UpdateProgress(7, $"{powerInWattHours / 3600000}Wh");
     }
 }
