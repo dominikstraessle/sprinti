5e4eca71556621e126fdd878941c04f707ca94d8
Dominik Strässle <dominik@straessle.me>
2024-07-07 10:55:50 +0200

    Update server addresses in flake.nix
    The server addresses used for nix copy commands in comments in flake.nix have been updated. The specific IPs/addresses have been replaced with a general 'server' placeholder to prevent hardcoding and improve the generality of the code.

--- flake.nix
+++ flake.nix
@@ -14,9 +14,9 @@
             inherit (self.packages.${system}) cvsharp;
           };
           # sprinti user needs to be in trusted-users in /etc/nix/nix.conf of the raspberry
-          # nix copy --to ssh-ng://sprinti@sprinti.secure.straessle.me .\#packages.aarch64-linux.sprinti
+          # nix copy --to ssh-ng://server .\#packages.aarch64-linux.sprinti
           # or
-          # nix copy --no-check-sigs --from ssh-ng://dominik@100.117.127.123 /nix/store/sxzvplbnl0m51qq5zkrc4jm4q4g8n3n2-sprinti-0.1
+          # nix copy --no-check-sigs --from ssh-ng://server /nix/store/sxzvplbnl0m51qq5zkrc4jm4q4g8n3n2-sprinti-0.1
           default = self.packages.${system}.sprinti;
         });
 
